9abc4a931ecd36a395586b37bd87d4035d8e0e67
Angela Harms <angela.harms@gmail.com>
2015-08-21 11:26:11 -0400

    Extract hello-response

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                [ring/ring-core "1.4.0"]]
+  :plugins [[lein-ring "0.9.6"]]
+  :ring {:handler alpha.core/app})
--- src/alpha/core.clj
+++ src/alpha/core.clj
@@ -1,6 +1,31 @@
 (ns alpha.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def hello-response  
+      {:headers {"content-type" "text/html"}
+       :body 
+        "<!DOCTYPE html>
+        <html>
+        <head><link href=\"style.css\" rel=\"stylesheet\"></head>
+        <body><p>Hello, World.</p></body>
+        </html>"})
+
+(defn app
+  "This is a learning app"
+  [request]
+  (cond
+    (= (:uri request) "/")
+      hello-response
+    (= (:uri request) "/foo")
+      {:body "Foo? Okaaaaaay."}
+    (= (:uri request) "/style.css")
+      {:headers {"content-type" "text/css"}
+       :body "p {color: red;} .sad {color: blue;}"}
+    :else
+      {:status 404
+       :headers {"content-type" "text/html"}
+       :body 
+        "<!DOCTYPE html>
+        <html>
+        <head><link href=\"style.css\" rel=\"stylesheet\"></head>
+        <body><p class=\"sad\">You've missed the boat.</p></body>
+        </html>"}))
