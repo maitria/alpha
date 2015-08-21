d1541f49287b933827d1681c1cb825664769de72
Angela Harms <angela.harms@gmail.com>
2015-08-21 11:35:28 -0400

    Extract more

--- src/alpha/core.clj
+++ src/alpha/core.clj
@@ -9,18 +9,7 @@
         <body><p>Hello, World.</p></body>
         </html>"})
 
-(defn app
-  "This is a learning app"
-  [request]
-  (cond
-    (= (:uri request) "/")
-      hello-response
-    (= (:uri request) "/foo")
-      {:body "Foo? Okaaaaaay."}
-    (= (:uri request) "/style.css")
-      {:headers {"content-type" "text/css"}
-       :body "p {color: red;} .sad {color: blue;}"}
-    :else
+(def no-such-page-response
       {:status 404
        :headers {"content-type" "text/html"}
        :body 
@@ -28,4 +17,23 @@
         <html>
         <head><link href=\"style.css\" rel=\"stylesheet\"></head>
         <body><p class=\"sad\">You've missed the boat.</p></body>
-        </html>"}))
+        </html>"})
+(def silly-response
+      {:body "Foo? Okaaaaaay."})
+
+(def css-response
+      {:headers {"content-type" "text/css"}
+       :body "p {color: red;} .sad {color: blue;}"})
+
+(defn app
+  "This is a learning app"
+  [request]
+  (cond
+    (= (:uri request) "/")
+      hello-response
+    (= (:uri request) "/foo")
+      silly-response
+    (= (:uri request) "/style.css")
+      css-response
+    :else
+      no-such-page-response))
